75d38877a9f79d3b79e0c6f981cf5c8a82ef71f7
Michael Telahun <mtm@trevi.et>
2022-03-29 21:17:22 +0300

    Update depends for profile_hrm and profile_payroll

--- profile_payroll/__manifest__.py
+++ profile_payroll/__manifest__.py
@@ -1,35 +1,35 @@
 # Copyright (C) 2019,2021 TREVI Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-{'name': 'Payroll Profile',
- 'summary': 'Complete payroll application',
- 'version': '14.0.1.0.0',
- 'author': 'Trevi Software',
- 'website': 'http://trevi.et',
- 'license': 'AGPL-3',
- 'category': 'Payroll',
- 'depends': [
-
-     # https://github.com/OCA/payroll
-     # https://github.com/trevi-software/trevi-hr
-     "hr_benefit_payroll",
-     "hr_contract_values_payroll",
-     "hr_employee_status_payroll",
-     "payroll_payslip_ammendment_contract_status",
-     "payroll_period_account",
-     "payroll_period_base_lock",
-     "payroll_period_contract_values",
-     "payroll_period_payslip_amendment",
-     "payroll_period_processing",
-     "payroll_policy_absence",
-     "payroll_policy_accrual",
-     "payroll_policy_ot",
-     "payroll_policy_presence",
-     "payroll_register",
- ],
- 'data': [
- ],
- 'installable': True,
- 'auto_install': True,
- 'application': True,
- }
+{
+    "name": "Payroll Profile",
+    "summary": "Complete payroll application",
+    "version": "14.0.1.0.0",
+    "author": "TREVI Software",
+    "images": ["static/src/img/main_screenshot.png"],
+    "website": "https://github.com/trevi-software/odoo-profiles",
+    "license": "AGPL-3",
+    "category": "Payroll",
+    "depends": [
+        # https://github.com/OCA/payroll
+        # https://github.com/trevi-software/trevi-hr
+        "hr_benefit_payroll",
+        "hr_contract_values_payroll",
+        "hr_employee_status_payroll",
+        "payroll_payslip_ammendment_contract_status",
+        "payroll_period_account",
+        "payroll_period_base_lock",
+        "payroll_period_contract_values",
+        "payroll_period_payslip_amendment",
+        "payroll_period_processing",
+        "payroll_policy_absence",
+        "payroll_policy_accrual",
+        "payroll_policy_ot",
+        "payroll_policy_presence",
+        "payroll_register",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+    "application": True,
+}
