e10d0be4fd71b34fbbb5992ccc65e40bd114f50c
Michael Telahun <mtm@trevi.et>
2022-03-31 11:08:54 +0300

    l10n_et_profile: Make module name more descriptive

--- l10n_et_profile_payroll/__manifest__.py
+++ l10n_et_profile_payroll/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Payroll Profile",
+    "name": "Ethiopia Payroll Profile",
     "summary": "Complete payroll application",
     "version": "14.0.1.0.0",
     "author": "TREVI Software",
