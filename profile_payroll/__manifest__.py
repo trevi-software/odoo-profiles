49416e98dca093921165af5f8ba12c92ef20f83b
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-22 20:07:10 +0300

    OOoops. Forgot to update module version numbers

--- profile_payroll/__manifest__.py
+++ profile_payroll/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Payroll Profile",
     "summary": "Complete payroll application",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "TREVI Software",
     "images": ["static/src/img/main_screenshot.png"],
     "website": "https://github.com/trevi-software/odoo-profiles",
